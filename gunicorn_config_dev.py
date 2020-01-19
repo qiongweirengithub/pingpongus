06ee2f5e0186e7d0f0a5ad51db1a997b265b58a7
renqiongwei <qiongwei_ren@163.com>
2020-01-20 01:32:46 +0800

    fix

---
+++ gunicorn_config_dev.py
@@ -0,0 +1,16 @@
+# config.py
+import os
+
+import multiprocessing
+
+# debug = True
+loglevel = 'debug'
+bind = "0.0.0.0:8080"
+pidfile = "/home/qiongwei/mycode/pythonenv/virtualenv/pingpongus/app/log/gunicorn.pid"
+accesslog = "/home/qiongwei/mycode/pythonenv/virtualenv/pingpongus/app/log/access.log"
+errorlog = "/home/qiongwei/mycode/pythonenv/virtualenv/pingpongus/app/log/debug.log"
+daemon = True
+
+# 启动的进程数
+workers = multiprocessing.cpu_count()
+x_forwarded_for_header = 'X-FORWARDED-FOR'
---
+++ gunicorn_config_release.py
@@ -0,0 +1,16 @@
+# config.py
+import os
+
+import multiprocessing
+
+# debug = True
+loglevel = 'debug'
+bind = "0.0.0.0:8080"
+pidfile = "/home/pingpongus/pythonserver/pingpongus/approot/log/gunicorn.pid"
+accesslog = "/home/pingpongus/pythonserver/pingpongus/approot/log/access.log"
+errorlog = "/home/pingpongus/pythonserver/pingpongus/approot/log/debug.log"
+daemon = True
+
+# 启动的进程数
+workers = multiprocessing.cpu_count()
+x_forwarded_for_header = 'X-FORWARDED-FOR'
